adfe102dfbbaf8b27e562eca65623179c491cdaf
firstmover <yingchengliu@pku.edu.cn>
2018-05-24 11:36:29 +0800

    add data position tag. use per ssid sniffing.

--- sniff_rssi.py
+++ sniff_rssi.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 import platform
-PLATFORM = platform.system() 
 import argparse
-import struct
 import json 
-import scapy.all as sca 
-from IPython import embed
+import scapy.all as sca
+
+PLATFORM = platform.system()
 
 
 class PktFilter(object):
@@ -19,52 +18,48 @@ class PktFilter(object):
         return pkt.haslayer(sca.Dot11Beacon) and pkt.ID == 0 \
                             and pkt.info in [i.encode('utf-8') for i in self.ssids]
 
-class ScapyRssi:
-    def __init__(self, ssids):
-        # data
-        self.data = {}
-        self.filter = PktFilter(ssids)
 
-    def sniff(self, interface, amount):
-        # TODO: why Linux do not support monitor=True?
-        if PLATFORM == 'Darwin':
-            packets = sca.sniff(iface=interface, lfilter=self.filter, count=amount, monitor=True)
-        elif PLATFORM == 'Linux':
-            packets = sca.sniff(iface=interface, lfilter=self.filter, count=amount)
-        else:
-            raise ValueError('unknown system.')
-        for pkt in packets:
-            ssid, rssi = self.parsePacket(pkt)
-            print(ssid, rssi)
-            if ssid is not None:
-                if ssid in self.data.keys():
-                    self.data[ssid].append(rssi)
-                else:
-                    self.data[ssid] = [rssi]
+def sniff_rssi(interface, ssid, amount):
+    # pkt filter
+    pkt_filter = PktFilter([ssid])
 
-    def parsePacket(self, pkt):
-        field, val = pkt.getfield_and_val("present")
+    # TODO: why Linux do not support monitor=True?
+    if PLATFORM == 'Darwin':
+        packets = sca.sniff(iface=interface, lfilter=pkt_filter, count=amount, monitor=True)
+    elif PLATFORM == 'Linux':
+        packets = sca.sniff(iface=interface, lfilter=pkt_filter, count=amount)
+    else:
+        raise ValueError('unknown system.')
+
+    def parse_packet(p):
+        field, val = p.getfield_and_val("present")
         names = [field.names[i] for i in range(len(field.names)) if (1 << i) & val != 0]
-        #print(names)
-        #print(pkt)
         # check if has signal strength field
         if "dBm_AntSignal" in names:
-            return str(pkt.info, encoding="utf-8"), -(256 - ord(pkt.notdecoded[-2:-1]))
-
+            return str(p.info, encoding="utf-8"), -(256 - ord(p.notdecoded[-2:-1]))
         return None, None
+
+    data = []
+    for pkt in packets:
+        pkt_ssid, rssi = parse_packet(pkt)
+        assert pkt_ssid == ssid
+        data.append(rssi)
+    return data
 
 
 def main():
     # argument parser 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", '-i', default='SSIDs.txt', required=True, type=str, 
-            help="input file containing list of Wi-Fi SSIDs, one SSID per line")
-    parser.add_argument("--output", '-o', default='result.txt', required=True, type=str, 
-            help="output file recording RSSIs")
-    parser.add_argument("--iface", '-if', required=True, type=str, 
-            help="NIC interface to sniff on, NOTE monitor mode must be open beforehand!")
-    parser.add_argument("--amount", '-a', default=100, type=int, 
-            help="amount of total beacon frames to be captured at once(default: 100)")
+    parser.add_argument("--input", '-i', default='SSIDs.txt', required=True, type=str,
+                        help="input file containing list of Wi-Fi SSIDs, one SSID per line")
+    parser.add_argument("--output", '-o', default='result.txt', required=True, type=str,
+                        help="output file recording RSSIs")
+    parser.add_argument("--iface", '-if', required=True, type=str,
+                        help="NIC interface to sniff on, NOTE monitor mode must be open beforehand!")
+    parser.add_argument("--amount", '-a', default=100, type=int,
+                        help="amount of total beacon frames to be captured at once(default: 100)")
+    parser.add_argument("--tag", '-t', required=True, type=str,
+                        help="tag for current position.")
     args = parser.parse_args()
 
     # only support MacOS and Linux 
@@ -74,25 +69,22 @@ def main():
     # read ssids from input file. 
     with open(args.input, "r") as i:
         ssids = [_.strip() for _ in i.readlines()]
-    print('SSIDs: {}'.format(ssids))
+    print('get SSIDs: {}'.format(ssids))
 
-    # do sniff
-    sniffer = ScapyRssi(ssids)
-    sniffer.sniff(args.iface, args.amount)
+    # sniff amount package for each ssid.
+    print("position tag: {}".format(args.tag))
+    data_dict = {'tag': args.tag}
+    for ssid in ssids:
+        print("sniffing ssid: {}".format(ssid))
+        data = sniff_rssi(args.iface, ssid, args.amount)
+        data_dict[ssid] = data
 
-    # debug
-    #print(sniffer.data)
     # save to json
+    print("saving to: {}".format(args.output))
     with open(args.output, "a") as o:
-        json.dump(sniffer.data, o)
-        o.write("\n")
+        o.write(json.dumps(data_dict) + '\n')
+
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-    
 
