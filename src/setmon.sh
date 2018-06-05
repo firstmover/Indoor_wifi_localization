9347b169aec2da8aec10d523f5509a69f1ebd7a8
firstmover <1500012974@pku.edu.cn>
2018-06-05 21:51:45 +0800

    organize files. seperate sniifer and visualizer.

--- src/setmon.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-service network-manager stop
-ifconfig wlp3s0 down
-iwconfig wlp3s0 mode monitor
-ifconfig wlp3s0 up
--- src/unsetmon.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ifconfig wlp3s0 down
-iwconfig wlp3s0 mode managed
-ifconfig wlp3s0 up
-service network-manager start
